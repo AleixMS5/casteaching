7d6601a8a519f9327a9160b35ebfea24b894edde
alumne <amontero@iesebre.com>
2023-03-13 19:25:51 +0100

    codi temporal video 130

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,21 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import casteaching_ams2 from '@ams092003/casteaching_ams2' ;
+import casteaching from '@acacha/casteaching' ;
 import Vue from 'vue/dist/vue.js';
 import VideosList from "./components/VideosList.vue";
 import VideoForm from "./components/VideoForm.vue";
-
+const baseUrl = import.meta.env.VITE_API_URL;
 import Status from "./components/Status.vue";
-
+const options = {baseUrl: baseUrl};
+const api = casteaching(options);
+console.log('options:');
+console.log(options);
 window.Alpine = Alpine;
-window.casteaching=casteaching_ams2;
+window.casteaching=api;
 Alpine.start();
-
-
+console.log("hola")
+console.log(baseUrl)
 const vueApp = document.querySelector('#app')
 
 if(vueApp){
